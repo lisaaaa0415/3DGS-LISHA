4f4fa9fc83376fed38876ce708d293201f6284ba
lisaaaa0415 <150511170+lisaaaa0415@users.noreply.github.com>
2024-05-16 21:41:33 +0800

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import json
+with open('config.json','r')as file:
+    data=json.load(file)
+    print(data)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
